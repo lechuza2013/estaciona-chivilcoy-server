2b7f494f9d84b7b18adb3a11fd9c84f9d058ea7e
Axel Julián Suarez <53792359+lechuza2013@users.noreply.github.com>
2023-08-06 16:30:20 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import * as bodyParser from "body-parser";
 import { v4 as uuidv4 } from "uuid";
 import * as cors from "cors";
 import { getMerchantOrder } from "./db";
-const fetch = require('node-fetch');
+import fetch from "node-fetch"
 
 const app = express();
 const PORT = 8080;
