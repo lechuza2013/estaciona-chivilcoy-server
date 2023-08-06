7369e52c6affc25d784e95fd3f3267e6b1c5ee59
Axel Julián Suarez <53792359+lechuza2013@users.noreply.github.com>
2023-08-06 01:34:08 -0300

    Update index.ts 6

--- server/index.ts
+++ server/index.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://estaciona-chivilcoy-j9mv.onrender.com/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
