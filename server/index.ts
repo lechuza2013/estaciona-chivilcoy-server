7477d0091f54c8edfcdf823a3f05bd4818871c32
Axel Julián Suarez <53792359+lechuza2013@users.noreply.github.com>
2023-08-06 00:50:24 -0300

    Update index.ts 5

--- server/index.ts
+++ server/index.ts
@@ -12,6 +12,14 @@ const mercadopago = require("mercadopago");
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const platesCollectionRef = firestoreDB.collection("carPlate");
 
